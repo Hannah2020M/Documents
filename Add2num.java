2b31724f0b87f45a0495de77df8ff1484ce8fd54
Hannah2020M <72173436+Hannah2020M@users.noreply.github.com>
2022-10-11 00:59:57 -0500

    Add files via upload
    Added file 2022 oct 11

---
+++ Add2num.java
@@ -0,0 +1,8 @@
+public class Add2num {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 14;
+        int sum2num = a + b;
+        System.out.println(" sum of "+a+"and "+b+" = "+sum2num);
+    }
+}
